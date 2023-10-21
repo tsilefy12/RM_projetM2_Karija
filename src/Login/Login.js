460579593a43fe3e7970f242c235adc6c5235819
tsilefy12 <tsilefyandriamalaza@gmail.com>
2023-10-21 21:44:08 +0300

    saika nahita faisana

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  TextField,InputAdornment } from '@mui/material';
+import { TextField, InputAdornment } from '@mui/material';
 import { FormLabel, Image } from 'react-bootstrap';
 import * as AiIcons from "react-icons/ai"
 import logo from '../images/FILE023.JPG';
@@ -7,54 +7,52 @@ import './Login.css';
 import { Link } from 'react-router-dom';
 import 'tachyons';
 function Login() {
-  
+
   return (
     <div className='tous'>
       <div className='logo'>
         <Image src={logo} id='img' width={320} height={308}></Image>
       </div>
-      <div className='flex login'>
-       <div className='flex' id='inpt'>
-       <FormLabel style={{margin: '20px'}} id='labelLog'>Adresse mail :</FormLabel>
-        <TextField
-          type='text'
-          sx={{ marginTop: '20px', label: { marginTop: '-10px', width: 'auto' } }} id='inputLog'
-          autoComplete='off'
-          placeholder='saisir votre adresse mail'
-          InputProps={
-            {
-              startAdornment: (
-                <InputAdornment position="start">
-                  <AiIcons.AiFillMail width={20} height={20} color='#800000'/>
-                </InputAdornment>
-              ),
+      <div className='login'>
+        <div className='flex' id='inpt'>
+          <FormLabel style={{ margin: '20px' }} id='labelLog'>Adresse mail :</FormLabel>
+          <TextField
+            type='text'
+            sx={{ marginTop: '20px', label: { marginTop: '-10px', width: 'auto' } }} id='inputLog'
+            autoComplete='off'
+            placeholder='saisir votre adresse mail'
+            InputProps={
+              {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AiIcons.AiFillMail width={20} height={20} color='#800000' />
+                  </InputAdornment>
+                ),
+              }
             }
-          }
-        />
-       </div>
-      <div className='flex' id='inpt'>
-      <FormLabel style={{margin: '20px'}} id='labelLog'>Mot de passe :</FormLabel>
-      <TextField
-          type='password'
-          sx={{ marginTop: '20px', label: { marginTop: '-10px' } }} id='inputLog'
-          autoComplete='off'
-          placeholder='saisir votre mot de passe'
-          InputProps={
-            {
-              startAdornment: (
-                <InputAdornment position="start">
-                  <AiIcons.AiTwotoneLock  width={20} height={20} color='#800000'/>
-                </InputAdornment>
-              ),
+          />
+        </div>
+        <div className='flex' id='inpt'>
+          <FormLabel style={{ margin: '20px' }} id='labelLog'>Mot de passe :</FormLabel>
+          <TextField
+            type='password'
+            sx={{ marginTop: '20px', label: { marginTop: '-10px' } }} id='inputLog'
+            autoComplete='off'
+            placeholder='saisir votre mot de passe'
+            InputProps={
+              {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AiIcons.AiTwotoneLock width={20} height={20} color='#800000' />
+                  </InputAdornment>
+                ),
+              }
             }
-          }
-        />
-      </div>
-        <div className='bouton-login'>
+          />
+        </div>
           <button className='btn btn-info grow' id='boutonLog'>Sign in</button>
           <Link to={'/inscription'} className='btn btn-success grow' id='boutonLog'>
             <button style={{ border: 'none' }} className='bg-success'>S'inscrire</button></Link>
-        </div>
       </div>
     </div>
   )
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
 import img from '../images/back_64px.png';
-import dash from '../icons/dashboard_layout_48px.png'
 import { Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Menu.css';
+import sousMenu from '../images/request_service_48px.png'
+import deletelogo from '../images/Delete_48px.png' 
+import paye from '../images/mobile_payment_48px.png'
 
 function Menu() {
   const [open, setOpen] = useState(false);
+  const [showServices, setShowServices] = useState(false);
 
   const handleImageClick = () => {
     setOpen(!open);
+  };
+  const toggleServices = () => {
+    setShowServices(!showServices);
   };
 
   const Menus = [
@@ -30,64 +36,89 @@ function Menu() {
   const afficher = !open ? 'none' : 'block';
   const topMenus = open ? '30px' : '50px';
   const magreLi = open ? '16px' : '20px';
-  const margeleft = !open ? '-17px': "-17px";
+  const margeleft = !open ? '-17px' : "-17px";
   return (
-      <div className='flex' expand="lg">
-        <div className={`toggle-menu ${open ? 'show' : ''}`}
+    <div className='flex' expand="lg">
+      <div className={`toggle-menu ${open ? 'show' : ''}`}
+        style={{
+          width: containerWidth,
+          position: 'fixed',
+          height: '100.5vh',
+          backgroundColor: '#8D0A22',
+          transition: 'width 0.5s',
+          left: 0,
+          zIndex: 1
+        }}
+      >
+        <Image
+          src={img}
           style={{
-            width: containerWidth,
-            position: 'fixed',
-            height: '100.5vh',
-            backgroundColor: '#8D0A22',
-            transition: 'width 0.5s',
-            left: 0, 
+            position: 'relative',
+            marginTop: top,
+            marginLeft: left,
+            width: '20px',
+            height: '20px',
+            cursor: 'pointer',
+            transition: 'margin-left 0.5s',
             zIndex: 1
           }}
-        >
-          <Image
-            src={img}
-            style={{
-              position: 'relative',
-              marginTop: top,
-              marginLeft: left,
-              width: '20px',
-              height: '20px',
-              cursor: 'pointer',
-              transition: 'margin-left 0.5s',
-              zIndex: 1
-            }}
-            onClick={handleImageClick}
-            className={`${!open && 'rotate-180'}`}
-          />
-          <div style={{
-            marginTop: topmenu, height: '90vh',
-            color: 'white', display: 'flex', flexDirection: 'column'
+          onClick={handleImageClick}
+          className={`${!open && 'rotate-180'}`}
+        />
+        <div style={{
+          marginTop: topmenu, height: '90vh',
+          color: 'white', display: 'flex', flexDirection: 'column'
+        }}>
+          <header className='header' style={{
+            width: "auto", height: "120px",
+            border: "1px solid white", marginTop: "-20px"
           }}>
-           <header className='header' style={{width:"auto", height:"120px", 
-           border:"1px solid white", marginTop: "-20px"}}>
-             Passager connecté
-           </header>
-              <ul style={{
-                marginTop: topMenus,borderTop: '4px solid #07af6f',
-                width: containerWidthMenu, transition: 'width 0.5s',
-                marginTop: "0",
-              }}>
-                {Menus.map((items, index) => (
-                  <div key={index} style={{ display: 'flex', flexDirection: 'row', marginLeft : margeleft, transition: 'margin-left 0.5s' }}>
-                    <Link to={items.path} className='flex' style={{ textDecoration: 'none', color: 'white' }}>
-                      <img src={items.icon} width={35} height={35} style={{borderRadius: "50%", backgroundColor: "#8D041D", marginTop:"10px"}}/>
-                      <li className='li' style={{
-                        display: afficher, transition: 'display: none 0.5s',
-                        textDecoration: 'none', marginTop: magreLi, marginLeft: "10px"
-                      }}>{items.title}</li>
-                    </Link>
-                  </div>
-                ))}
-              </ul>
-             
-          </div>  
+            Passager connecté
+          </header>
+          <ul style={{
+            marginTop: topMenus, borderTop: '4px solid #07af6f',
+            width: containerWidthMenu, transition: 'width 0.5s',
+            marginTop: "0",
+          }}>
+            {Menus.map((items, index) => (
+              <div key={index} style={{ display: 'flex', flexDirection: 'row', marginLeft: margeleft, transition: 'margin-left 0.5s' }}>
+                <Link to={items.path} className='flex' style={{ textDecoration: 'none', color: 'white' }}>
+                  <img src={items.icon} width={35} height={35} style={{ borderRadius: "50%", backgroundColor: "#8D041D", marginTop: "10px" }} />
+                  <li className='li' style={{
+                    display: afficher, transition: 'display: none 0.5s',
+                    textDecoration: 'none', marginTop: magreLi, marginLeft: "10px"
+                  }}>{items.title}</li>
+                </Link>
+              </div>
+            ))}
+            <Link onClick={toggleServices} className='sous-menu flex'>
+                <img src={sousMenu} width={35} height={35} style={{ borderRadius: "50%", backgroundColor: "#8D041D", marginTop: "10px", marginLeft: '-20px' }}>
+                </img>
+                <label className='li' style={{
+                    display: afficher, transition: 'display: none 0.5s',
+                    textDecoration: 'none', marginTop: magreLi, marginLeft: "10px", color: 'white', cursor: 'pointer'}}>DEMANDE</label>
+              </Link>
+              {showServices && (
+                <ul style={{marginLeft: '-40px'}}>
+                  <Link className='flex' style={{width: '350px', textDecoration: 'none'}}>
+                   <Image src={deletelogo} width={25} height={25}></Image>
+                    <label style={{
+                    display: afficher, transition: 'display: none 0.5s',
+                    textDecoration: 'none', marginLeft: "8px", color: 'white', cursor: 'pointer'}}>ANNULATION</label>
+                  </Link>
+                  <Link className='flex' style={{width: '350px', textDecoration: 'none'}}>
+                    <Image src={paye} width={25} height={25}></Image>
+                    <label style={{
+                    display: afficher, transition: 'display: none 0.5s',
+                    textDecoration: 'none', marginLeft: "8px", color: 'white', cursor: 'pointer'}}>REMBOURSEMENT</label>
+                  </Link>
+                </ul>
+              )}
+          </ul>
+         
         </div>
       </div>
+    </div>
   );
 }
 export default Menu;
